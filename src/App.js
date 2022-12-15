9b395214f3debf56b3f1eadfc9085f54d737257f
MeaBoy656 <MeaBoy656@gmail.com>
2022-12-15 21:24:53 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img
+          id="logo"
+          src={process.env.PUBLIC_URL + "./logo.png"}
+          alt="לוגו בסמח"
+        />
+        <h1 className="title">בסמח אלפא</h1>
+        <h2 className="subTitle">חידת חנוכה</h2>
       </header>
+      <figure>
+        
+      </figure>
+      <main>
+        <p className="text">The universe is big. It’s vast and complicated and ridiculous. And sometimes, very rarely, impossible things just happen and we call them miracles</p>
+      </main>
     </div>
   );
 }
